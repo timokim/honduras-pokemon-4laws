7b163df7a58d8f4e75e4a2b41f5d904bf339e2a6
Timothy Kim <timothy.kim@mac-1213-tk.local>
2018-04-07 16:58:33 -0400

    Randomly Moving Satan Implemented

--- pylletTown.py
+++ pylletTown.py
@@ -1,5 +1,6 @@
 import pygame
 import tmx
+import random
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, location, orientation, *groups):
@@ -65,13 +66,13 @@ class Player(pygame.sprite.Sprite):
         # Walking at 8 pixels per frame in the direction the player is facing 
         if self.walking and self.dx < 64:
             if self.orient == 'up':
-                self.rect.y += 8
-            elif self.orient == 'down':
                 self.rect.y -= 8
+            elif self.orient == 'down':
+                self.rect.y += 8
             elif self.orient == 'left':
-                self.rect.x += 8
-            elif self.orient == 'right':
                 self.rect.x -= 8
+            elif self.orient == 'right':
+                self.rect.x += 8
             self.dx += 8
         # Collision detection:
         # Reset to the previous rectangle if player collides
@@ -105,6 +106,117 @@ class Player(pygame.sprite.Sprite):
             self.dx = 0
         
         game.tilemap.set_focus(self.rect.x, self.rect.y)
+
+# class EdibleHearts(pygame.sprite.Sprite):
+#     def __init__(self, )
+
+class Satan(pygame.sprite.Sprite):
+    def __init__(self, location, orientation, *groups):
+        super(Satan, self).__init__(*groups)
+        self.image = pygame.image.load('sprites/satan.png')
+        self.imageDefault = self.image.copy()
+        self.rect = pygame.Rect(location, (64,64))
+        self.orient = orientation
+        self.holdTime = 0
+        self.walking = False
+        self.dx = 0
+        self.step = 'rightFoot'
+        # Set default orientation
+        self.setSprite()
+
+    def setSprite(self):
+        # Resets the player sprite sheet to its default position 
+        # and scrolls it to the necessary position for the current orientation
+        self.image = self.imageDefault.copy()
+        if self.orient == 'up':
+            self.image.scroll(0, -64)
+        elif self.orient == 'down':
+            self.image.scroll(0, 0)
+        elif self.orient == 'left':
+            self.image.scroll(0, -128)
+        elif self.orient == 'right':
+            self.image.scroll(0, -192)
+
+    def random_move(self, dt, game):
+        key = random.randint(1,5)
+        # Setting orientation and sprite based on key input: 
+        if key == 1:
+            if not self.walking:
+                if self.orient != 'up':
+                    self.orient = 'up'
+                    self.setSprite()
+                self.holdTime += dt
+        elif key == 2:
+            if not self.walking:
+                if self.orient != 'down':
+                    self.orient = 'down'
+                    self.setSprite()    
+                self.holdTime += dt
+        elif key == 3:
+            if not self.walking:
+                if self.orient != 'left':
+                    self.orient = 'left'
+                    self.setSprite()
+                self.holdTime += dt
+        elif key == 4:
+            if not self.walking:
+                if self.orient != 'right':
+                    self.orient = 'right'
+                    self.setSprite()
+                self.holdTime += dt
+        else:
+            self.holdTime = 0
+            self.step = 'rightFoot'
+        # Walking mode enabled if a button is held for 0.1 seconds
+        if self.holdTime >= 0:
+            self.walking = True
+        lastRect = self.rect.copy()
+        # Walking at 8 pixels per frame in the direction the player is facing 
+        if self.walking and self.dx < 64:
+            if self.orient == 'up':
+                self.rect.y -= 8
+            elif self.orient == 'down':
+                self.rect.y += 8
+            elif self.orient == 'left':
+                self.rect.x -= 8
+            elif self.orient == 'right':
+                self.rect.x += 8
+            self.dx += 8
+        # Collision detection:
+        # Reset to the previous rectangle if player collides
+        # with anything in the foreground layer
+        if len(game.tilemap.layers['triggers'].collide(self.rect, 
+                                                        'solid')) > 0:
+            self.rect = lastRect
+        # Area entry detection:
+        elif len(game.tilemap.layers['triggers'].collide(self.rect, 
+                                                        'entry')) > 0:
+            entryCell = game.tilemap.layers['triggers'].find('entry')[0]
+            game.fadeOut()
+            game.initArea(entryCell['entry'])
+            
+            return
+        # Switch to the walking sprite after 32 pixels 
+        if self.dx == 32:
+            # Self.step keeps track of when to flip the sprite so that
+            # the character appears to be taking steps with different feet.
+            if (self.orient == 'up' or 
+                self.orient == 'down') and self.step == 'leftFoot':
+                self.image = pygame.transform.flip(self.image, True, False)
+                self.step = 'rightFoot'
+            else:
+                self.image.scroll(-64, 0)
+                self.step = 'leftFoot'
+        # After traversing 64 pixels, the walking animation is done
+        if self.dx == 64:
+            self.walking = False
+            self.setSprite()    
+            self.dx = 0
+        
+        # game.tilemap.set_focus(self.rect.x, self.rect.y)
+        
+    def update(self, dt, game):
+        self.random_move(dt, game)
 
 class SpriteLoop(pygame.sprite.Sprite):
     """A simple looped animated sprite.
@@ -181,12 +293,14 @@ class Game(object):
         startCell = self.tilemap.layers['triggers'].find('playerStart')[0]
         self.player = Player((startCell.px, startCell.py), 
                              startCell['playerStart'], self.players)
+        self.satan = Satan((startCell.px-64, startCell.py-64), 
+                             startCell['playerStart'], self.players)
         self.tilemap.layers.append(self.players)
         self.tilemap.set_focus(self.player.rect.x, self.player.rect.y)  
             
     def main(self):
         clock = pygame.time.Clock()
-        self.initArea('palletTown.tmx')
+        self.initArea('FirstLaw.tmx')
         
         while 1:
             dt = clock.tick(30)
